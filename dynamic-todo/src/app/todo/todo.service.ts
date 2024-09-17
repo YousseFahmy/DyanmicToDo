4ab1bed7956cd78bed5952908cf03f8c926ad896
Youssef Mahmoud <ymahmoud@sumerge.com>
2024-09-17 18:47:43 +0300

    Implemented add & complete functionalities

--- dynamic-todo/src/app/todo/todo.service.ts
+++ dynamic-todo/src/app/todo/todo.service.ts
@@ -1,6 +1,6 @@
 import { inject, Injectable, signal } from "@angular/core";
 import { TodoItem } from "./todoItem.model";
-import { collection, collectionData, Firestore } from "@angular/fire/firestore";
+import { collection, collectionData, Firestore, addDoc, setDoc, doc } from "@angular/fire/firestore";
 import { Observable, tap } from "rxjs";
 
 @Injectable({ providedIn: "root" })
@@ -19,25 +19,23 @@ export class TodoService {
     }
 
     addItem(newItemText: string) {
-        var newItem: TodoItem | any = {
+        var newItem = {
             text: newItemText,
-            // id: Math.random(),
             isComplete: false
         };
 
-        this.items.update(oldItems => {
-            return [...oldItems, newItem];
-        });
+        addDoc(this.todoCollection, newItem);
     }
 
     completeItem(itemId: string) {
         this.items.update(oldItems => {
-            console.log(oldItems);
             return oldItems.map(item => {
                 if (item.id !== itemId) return item;
                 return { ...item, isComplete: true };
             });
         });
 
+        const docRef = doc(this.todoCollection, itemId);
+        setDoc(docRef, { isComplete: true }, { merge: true });
     }
 };
